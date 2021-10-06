19728cb97bcff03954b175a21f62a587ec8a0458
Nathan Mejia <natemejia3@gmail.com>
2021-10-05 19:33:42 -0500

    holy crap it works now

--- e.py
+++ e.py
@@ -6,28 +6,36 @@ from scipy.ndimage import convolve
 
 rose = mpimg.imread('rose-piano.jpg')
 
-blur = [[
+rose1 = rose / 255
+
+blur = [[[1/9, 1/9, 1/9],
     [1/9, 1/9, 1/9],
-    [1/9, 1/9, 1/9],
-    [1/9, 1/9, 1/9]
-]]
+    [1/9, 1/9, 1/9]]]
+
 blur = np.array(blur)
-blur /= np.sum(blur)
-a = convolve(rose, blur)
+a = convolve(rose1, blur, output=None, mode='reflect', cval=0.0, origin=0)
 
 b = cv2.GaussianBlur(rose, (5, 5), sigmaX=0)
 
-edge = [[
+edge = [
     [-1, -1, -1],
     [-1, 8, -1],
     [-1, -1, -1]
-]]
+]
 edge = np.array(edge)
-c = convolve(rose, edge)
+rose1 = np.mean(rose1, axis=2)
+c = convolve(rose1, edge)
+c = np.where(c > 0.25, 1, 0)
 
 d = cv2.Laplacian(rose, cv2.CV_8U, ksize=3)
 
 e = cv2.Laplacian(b, cv2.CV_8U, ksize=3)
 
-plt.imshow(c)
+#plt.imsave('exercise5a.jpg', a)
+plt.imsave('exercise5b.jpg', b)
+plt.imsave('exercise5c.jpg', c)
+plt.imsave('exercise5d.jpg', d)
+plt.imsave('exercise5e.jpg', e)
+
+plt.imshow(a)
 plt.show()
